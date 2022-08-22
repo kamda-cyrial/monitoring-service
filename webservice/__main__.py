74efd3865bb6bbc0157a91bb4bae927022e3a256
kamda-cyrial <42605286+kamda-cyrial@users.noreply.github.com>
2022-08-22 05:40:57 -0400

    fixing the indent issue

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -39,7 +39,7 @@ async def issue_is_labeled(event, gh, *args, **kwargs):
 
 @router.register("pull_request", action="closed")
 async def pull_request_closed(event, gh, *args, **kwargs):
-    print(json.dumps(event.data, line_indent=4))
+    print(json.dumps(event.data, indent=4))
     url = event.data["comments_url"]
     author = event.data["pull_request"]["user"]["login"]
     merge_status = event.data["merged"]
